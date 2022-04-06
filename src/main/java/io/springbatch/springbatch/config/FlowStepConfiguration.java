5ca38e3fac9842b0c18f85b84bcb52e61832c3d0
215725 <tmkim@shinsegae.com>
2022-04-06 13:37:39 +0900

    Custom ExitStatus TEST (Listener를 이용하여 ExitStatus 설정 가능, 실패가 아니지만 지정되지 않은 상태 처리하기 위함)

--- src/main/java/io/springbatch/springbatch/config/FlowStepConfiguration.java
+++ src/main/java/io/springbatch/springbatch/config/FlowStepConfiguration.java
@@ -1,11 +1,13 @@
 package io.springbatch.springbatch.config;
 
+import io.springbatch.springbatch.listener.PassCheckingListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -23,8 +25,9 @@ public class FlowStepConfiguration {
     @Bean
     public Job flowJob() {
         return this.jobBuilderFactory.get("flowJob")
+                .incrementer(new RunIdIncrementer())
                 .start(flowStep1())
-                .on("COMPLETED").to(flowStep3())
+                .on("COMPLETED").to(flowStep3()).on("PASS").stop()
                 .from(flowStep1())
                 .on("FAILED").to(flowStep2())
                 .end()
@@ -38,8 +41,7 @@ public class FlowStepConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         log.info("step1 has executed");
-                        throw new RuntimeException("step1 was failed");
-//                        return RepeatStatus.FINISHED;
+                        return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
@@ -68,6 +70,7 @@ public class FlowStepConfiguration {
                         return RepeatStatus.FINISHED;
                     }
                 })
+                .listener(new PassCheckingListener())
                 .build();
     }
 }
