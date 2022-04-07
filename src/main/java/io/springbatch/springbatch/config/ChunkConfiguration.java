114357e158f964de37e00b9ef4634dba83a8f68a
215725 <tmkim@shinsegae.com>
2022-04-07 09:10:02 +0900

    Chunk 기능 테스트

---
+++ src/main/java/io/springbatch/springbatch/config/ChunkConfiguration.java
@@ -0,0 +1,77 @@
+package io.springbatch.springbatch.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ChunkConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    @Primary
+    public Job chunkJob() {
+        return this.jobBuilderFactory.get("chunkJob")
+                .incrementer(new RunIdIncrementer())
+                .start(chunkStep1())
+                .next(chunkStep2())
+                .build();
+    }
+
+    @Bean
+    public Step chunkStep1() {
+        return this.stepBuilderFactory.get("chunkStep1")
+                //chunk API 사용
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "itme2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        Thread.sleep(300);
+                        log.info("item = " + item);
+                        return "my " + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        Thread.sleep(300);
+                        log.info("write items" + items);
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step chunkStep2() {
+        return this.stepBuilderFactory.get("chunkStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        log.info(">> chunkStep2 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
